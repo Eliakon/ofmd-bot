e1ffa9132b8820507de4e39276eab68b5d3e32e1
Estelle Martinez <rikku.shimizu@gmail.com>
2023-12-01 12:03:33 -0500

    Makes bot connect to Discord

---
+++ ofmd-bot.py
@@ -0,0 +1,18 @@
+# coding:utf8
+
+import os
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+    print(f'{client.user.name} has connected to Discord!')
+
+client.run(TOKEN)# coding:utf8
