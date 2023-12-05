77603e4d36b60a561b0e72ea8742e476df96dfe9
Estelle Martinez <rikku.shimizu@gmail.com>
2023-12-05 10:34:42 -0500

    Fix next schedule target time

--- ofmd-bot.py
+++ ofmd-bot.py
@@ -2,8 +2,7 @@
 
 import os, asyncio
 import discord
-from discord.ext import commands, tasks
-from datetime import datetime, timedelta
+from discord.ext import tasks
 from dotenv import load_dotenv
 import moment
 
@@ -20,11 +19,18 @@ async def on_ready():
     print(f'{client.user.name} has connected to Discord!')
     send_tuesday_img.start()
 
+
 def get_next_scheduled_time():
-    target_time = moment.utcnow().zero.add(day=1).replace(hours=5)
+    target_time = moment.utcnow().zero.replace(hours=16)
+    
+    if target_time < moment.utcnow():
+        target_time.add(day=1)
+
     while target_time.weekday != 2:
         target_time.add(day=1)
+    
     return target_time
+
 
 @tasks.loop(seconds=1)
 async def send_tuesday_img():
@@ -36,5 +42,6 @@ async def send_tuesday_img():
     embed = discord.Embed()
     embed.set_image(url=TUESDAY_IMG_URL)
     await channel.send(embed=embed)
+
 
 client.run(TOKEN)
